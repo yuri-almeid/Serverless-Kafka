6d33fd717615a51c0b47cf8a01c92d1e4ecc40cd
Yuri Almeida <yurilima95@gmail.com>
2022-01-24 19:42:43 -0300

    demo almost done

--- README.md
+++ README.md
@@ -1,17 +1,14 @@
-# Codingly.io: Base Serverless Framework Template
+# A Kafka Serverless
 
-https://codingly.io
-
-## What's included
-* Folder structure used consistently across our projects.
-* [serverless-pseudo-parameters plugin](https://www.npmjs.com/package/serverless-pseudo-parameters): Allows you to take advantage of CloudFormation Pseudo Parameters.
-* [serverless-bundle plugin](https://www.npmjs.com/package/serverless-pseudo-parameters): Bundler based on the serverless-webpack plugin - requires zero configuration and fully compatible with ES6/ES7 features.
 
 ## Getting started
 ```
-sls create --name YOUR_PROJECT_NAME --template-url https://github.com/codingly-io/sls-base
-cd YOUR_PROJECT_NAME
 npm install
+```
+
+To deploy just do
+```
+sls deploy
 ```
 
 You are ready to go!
--- src/handlers/getAccess.js
+++ src/handlers/getAccess.js
@@ -2,39 +2,34 @@ const { Kafka } = require("kafkajs");
 
 async function getAccess(event, context) {
 
-  const clientId = "SCA-SNACKIN";
-  
-	// const brokers = ["localhost:9092"];
+	const clientId = "SCA-SNACKIN";
 	const brokers = ["https://snackin-log.loca.lt"];
-  
-	const topic = "SNACKIN_IOT_LOGS ";
+	const topic = "SNACKIN_IOT_LOGS";
 
-  const kafka = new Kafka({ clientId, brokers });
-  const producer = kafka.producer();
+	const kafka = new Kafka({ clientId, ssl: true, brokers });
+	const producer = kafka.producer();
 
 	var msg = "";
-  try {
-	  await producer.send({
-		  topic,
-		  messages: [
-			  {
-				  key: String(i),
-				  value: "Hello from lambda"
-			  }
-		  ],
-	  });
+	try {
+		await producer.connect();
+		await producer.send({
+			topic,
+			messages: [
+				{
+					key: String(Math.floor(Math.random() * 100)),
+					value: "Hello from lambda"
+				}
+			],
+		});
 		msg = "Message sent!";
-
-  } catch (err) {
-	  msg = "could not write message " + err;
-	  console.error(msg);
-  }
-
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: msg }),
-  };
+	} catch (err) {
+		msg = "could not write message " + err;
+		console.error(msg);
+	}
+	return {
+		statusCode: 200,
+		body: JSON.stringify({ message: msg }),
+	};
 }
 
 export const handler = getAccess;
